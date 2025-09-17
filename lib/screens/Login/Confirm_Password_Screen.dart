2c8a60200c2d61ebb59032921c9e2a25a3eb647e
ryuhochan0406 <yoo020406@naver.com>
2025-09-17 09:24:25 +0900

    대표 아이콘, 화면 배경색

--- lib/screens/Login/Confirm_Password_Screen.dart
+++ lib/screens/Login/Confirm_Password_Screen.dart
@@ -47,7 +47,11 @@ class _ConfirmPasswordScreenState extends State<ConfirmPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black87),
         leading: IconButton(
           onPressed: _goBack,
           icon: const Icon(Icons.arrow_back_ios_new),
--- lib/screens/Login/Nickname_Input_Screen.dart
+++ lib/screens/Login/Nickname_Input_Screen.dart
@@ -80,7 +80,11 @@ class _NicknameInputScreenState extends State<NicknameInputScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black87),
         leading: IconButton(onPressed: _goBack, icon: const Icon(Icons.arrow_back_ios_new)),
       ),
       body: Column(
